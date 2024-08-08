129bf033b4cf36fc8a25ca8b04ac4d426214aef9
Caepena <caepenafiel@gmail.com>
2024-08-08 12:10:13 -0300

    Funções com Arrays e Listas

--- js/script.js
+++ js/script.js
@@ -21,3 +21,70 @@ console.log("==============LOOP FOR IN==============");
 for (let i in frutas) {
     console.log(i);
 }
+
+// ForEach não para com continue, break...
+console.log("==============LOOP FOR EACH==============");
+frutas.forEach(function(f, indice, array){
+    console.log("ITEM", indice, f)
+});
+
+// Recuperar a lista ul que está no HTML de id 'lista'
+let lista = document.getElementById("lista");
+
+
+document.getElementById("btnAdd").addEventListener("click", ()=>{
+    // Recuperando o input onde o nome da fruta é digitado
+    let inputFruta = document.getElementById("idFruta");
+
+    // Passando o valor digitado no campo para o método push()
+    frutas.push(inputFruta.value); // Método que coloca o valor do input no final da lista
+    // frutas.unshift(inputFruta.value); // Esse método inseri o valor no início da lista, não no final
+    renderizaLista();
+    inputFruta.value = "";
+});
+
+// document.getElementById("btnDel").addEventListener("click", () => {
+//     frutas.pop(); //Remove um item do final da lista.
+//     // frutas.shift(); //Remove um item do início da lista.
+//     renderizaLista();
+// });
+
+document.getElementById("btnDel").addEventListener("click", () => {
+    //Recuperando o input onde o nome da fruta é digitado.
+    let inputFruta = document.getElementById("idFruta");
+ 
+    //Passando o valor digitado no campo para o método indexOf para decobrir indice do elemento.
+    let indice = frutas.indexOf(inputFruta.value);
+    if (indice > -1) {
+        frutas.splice(indice, 1);
+        renderizaLista();
+    } else {
+        alert("Item não existe na lista!")
+    }
+    inputFruta.value = "";
+});
+
+
+document.getElementById("btnOrdem").addEventListener("click", () => {
+    frutas.sort(); // Ordena em ordem alfabética
+    renderizaLista();
+});
+
+document.getElementById("btnReverte").addEventListener("click", () => {
+    frutas.reverse();//Reverte a ordem atual.
+    renderizaLista();
+});
+
+function renderizaLista(){
+    lista.innerHTML = ""
+    frutas.forEach(fruta => {
+        // Criando elementos li para adicionar na lista
+        let elementoLi = document.createElement("li");
+        // Criando texto para adicionar no elemento li
+        elementoLi.textContent = fruta;
+        // Adicionar o elemento li na lista
+        lista.appendChild(elementoLi)
+    })
+}
+
+renderizaLista();
