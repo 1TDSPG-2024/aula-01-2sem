3af0cd4de3c1f13ba38412f1bb9b64dd13335335
LeonardoIafrate <leonardoiafrate01@gmail.com>
2024-08-08 12:10:01 -0300

    adicionando e configurando botões

--- js/script.js
+++ js/script.js
@@ -39,20 +39,63 @@ frutas.forEach( function(f, indice, array){
 //     console.log(frase[i]);
 // }
 
-// document.getElementById("btnAdd").addEventListener("click", function(){
-//     console.log(this.textContent);
-// })
 
 //Recuperar a lista UL que está no HTML de id 'lista'
 let lista = document.getElementById("lista")
 
-frutas.forEach( fruta=>{
-    //Criando elementos li para adicioanar a lista.
-    let elementoLi = document.createElement("li");
-    //Criando texto para adicionar ao elemento li
-    elementoLi.textContent = fruta;
-    //Adiciona o elemento a lista
-    lista.appendChild(elementoLi);
+document.getElementById("btnAdd").addEventListener("click", () => {
+    let inputFruta = document.getElementById("idFruta");
+    
+    // frutas.push(inputFruta.value);Insere no final da lista
+    frutas.unshift(inputFruta.value);//Insere no início da lista
+    renderizaLista();
+    inputFruta.value = "";
 })
 
+// document.getElementById("btnAdd").addEventListener("click", () => {
+//     //Recuperando o input onde o nome da fruta  é digitado.
+//     let inputFruta = document.getElementById("idFruta");
 
+//     //Passando o valor digitado no campo para o método PUSH()
+//     frutas.push(inputFruta.value);
+//     renderizaLista()
+//     inputFruta.value = ""
+// });
+
+document.getElementById("btnDel").addEventListener("click", () => {
+    //Recuperando o input onde o nome da fruta é digitado.
+    let inputFruta = document.getElementById("idFruta");
+
+    //Passando o valor digitado no campo para o método indexOf para decobrir indice do elemento.
+    let indice = frutas.indexOf(inputFruta.value);
+    if (indice > -1) {
+        frutas.splice(indice, 1);
+        renderizaLista();
+    }
+    inputFruta.value = "";
+});
+ 
+ document.getElementById("btnOrdem").addEventListener("click", () => {
+    frutas.sort();//Coloca em ordem alfabética
+    renderizaLista();
+});
+
+
+ document.getElementById("btnReverte").addEventListener("click", () => {
+    frutas.reverse();//Reverte a ordem atual.
+    renderizaLista();
+});
+
+ function renderizaLista() {
+    lista.innerHTML = "";
+    frutas.forEach(fruta => {
+        //Criando elementos li para adicionar na lista.
+        let elementoLi = document.createElement("li");
+        //Criando texto para adicionar no elemento li.
+        elementoLi.textContent = fruta;
+        //Adicionar o elemento li na lista.
+        lista.appendChild(elementoLi);
+    });
+}
+
+renderizaLista();
